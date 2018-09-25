909e79682aa5526039b9e95c6e41dddf8421d0db
[zhumingliang] <[mingliangzhu@aliyun.com]>
2018-09-25 23:33:12 +0800

    测试接受数据

--- application/api/service/ReceiveService.php
+++ application/api/service/ReceiveService.php
@@ -20,40 +20,37 @@ class ReceiveService
      * @param $msg_arr
      * @throws \Exception
      */
-    public static function save($msg_arr){
-        TestT::create(['msg'=> json_encode($msg_arr)]);
-        TestT::create(['msg'=> count($msg_arr)]);
-
-        if (count($msg_arr) == 1) {
-            if (isset($msg_arr['login_type'])) {
-                LoginT::create($msg_arr);
-
-            } else {
-                ReceiveT::create($msg_arr);
-            }
-
+    public static function save($msg_arr)
+    {
+        //if (count($msg_arr) == 1) {
+        if (isset($msg_arr['login_type'])) {
+            LoginT::create($msg_arr);
         } else {
-            $data_login = [];
-            $data_receive = [];
-            foreach ($msg_arr as $v) {
-                if (isset($v['login_type'])) {
-                    array_push($data_login, $v);
-
-                } else {
-                    array_push($data_receive, $v);
-                }
-            }
-            if (count($data_login)) {
-                $login = new LoginT();
-                $login->saveAll($data_login);
-
-            }
-            if (count($data_receive)) {
-                $login = new LoginT();
-                $login->saveAll($data_receive);
-
-            }
+            ReceiveT::create($msg_arr);
         }
+        /*
+                } else {
+                    $data_login = [];
+                    $data_receive = [];
+                    foreach ($msg_arr as $v) {
+                        if (isset($v['login_type'])) {
+                            array_push($data_login, $v);
+
+                        } else {
+                            array_push($data_receive, $v);
+                        }
+                    }
+                    if (count($data_login)) {
+                        $login = new LoginT();
+                        $login->saveAll($data_login);
+
+                    }
+                    if (count($data_receive)) {
+                        $login = new LoginT();
+                        $login->saveAll($data_receive);
+
+                    }
+                }*/
     }
 
 }
