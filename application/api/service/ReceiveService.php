5853cf42f124bb4ed44d9f91ccb1dca07f4d657a
[zhumingliang] <[mingliangzhu@aliyun.com]>
2018-09-28 18:46:02 +0800

    测试接受数据

--- application/api/service/ReceiveService.php
+++ application/api/service/ReceiveService.php
@@ -76,7 +76,7 @@ class ReceiveService
     {
         try {
             //$url = 'http://api.heclouds.com/devices/44631936/datapoints?type=1';
-            $url = "http://api.heclouds.com/nbiot?imei=865820031313187&obj_id=3300&obj_inst_id=0&mode=2";
+            $url = "http://api.heclouds.com/nbiot?imei=865820031313187&obj_id=3300&obj_inst_id=0&mode=2 HTTP/1.1";
             //*****处填写自己的设备ID号
             $header[] = "api-key:MRee0TFqxdtK2bsbyiFLgpmukSY=";
             $header[] = "Content-Type:application/json";
