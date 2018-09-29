a3ad6bc906e2967adffd5463513e3d38210bd2a1
[zhumingliang] <[mingliangzhu@aliyun.com]>
2018-09-29 23:07:14 +0800

    测试接受数据

---
+++ public/api/v1/api_project.js
@@ -0,0 +1,1 @@
+define({  "name": "盟蚁球馆API",  "version": "1.1.0",  "description": "盟蚁球馆API接口文档",  "title": "盟蚁球馆API",  "url": "http://mengant.cn",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-09-29T15:06:47.185Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
