031a67c9593e3c0edd87814e247a8b9aaa40ec34
[zhumingliang] <[mingliangzhu@aliyun.com]>
2018-10-07 13:43:12 +0800

    测试接受数据

--- application/api/service/ReceiveService.php
+++ application/api/service/ReceiveService.php
@@ -103,6 +103,88 @@ class ReceiveService
     }
 
 
+    /**
+     *向传感器发送数据
+     * @param $params
+     * @return mixed
+     */
+    public static function sendToOneNet($params)
+    {
+        try {
+            $sendParams = self::preParams($params['imei'], $params['obj_id'], $params['obj_inst_id'],
+                $params['res_id'], $params['X'], $params['Y'], $params['threshold'], $params['interval']);
+            $output = self::post($sendParams['url'], $sendParams['header'], $sendParams['content']);
+            LogT::create(['msg' => $output]);
+            $output_array = json_decode($output, true);
+            return $output_array;
+        } catch (Exception $e) {
 
+            LogT::create(['msg' => $e->getMessage()]);
+        }
+    }
+
+    public static function post($url, $header, $content)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        //TRUE-->将curl_exec()获取的信息以字符串返回，而不是直接输出。
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+        //启用时会将头文件的信息作为数据流输出
+        curl_setopt($ch, CURLOPT_POST, true);
+        //启用时会发送一个常规的POST请求，类型为：application/x-www-form-urlencoded，就像表单提交的一样
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $content);
+        if (curl_exec($ch) === false) //curl_error()返回当前会话最后一次错误的字符串
+        {
+            die("Curlerror: " . curl_error($ch));
+        }
+        $response = curl_exec($ch);
+        //获取返回的文件流
+        curl_close($ch);
+        return $response;
+    }
+
+
+    /**
+     * 准备数据
+     * @param string $imei
+     * @param int $obj_id
+     * @param int $obj_inst_id
+     * @param int $res_id
+     * @param $X
+     * @param $Y
+     * @param $threshold
+     * @param $interval
+     * @return array
+     */
+    private static function preParams($imei, $obj_id, $obj_inst_id, $res_id,
+                                      $X, $Y, $threshold, $interval)
+    {
+        $url = config('onenet.send_url');
+        $url = sprintf($url, $imei, $obj_id, $obj_inst_id);
+
+        $header[] = "api-key: MRee0TFqxdtK2bsbyiFLgpmukSY=";
+        $header[] = "Content-Type: application/json";
+        $header[] = "Host: api.heclouds.com";
+
+        $val = [$X, $Y, $threshold, $interval, "A"];
+        $val = implode('A', $val);
+
+        $content = new \stdClass();
+        $param = new \stdClass();
+        $param->res_id = $res_id;
+        $param->val = $val;
+        $content->data = [
+            0 => $param
+        ];
+
+        $content = json_encode($content);
+        return [
+            'url' => $url,
+            'header' => $header,
+            'content' => $content,
+
+        ];
+    }
 
 }
