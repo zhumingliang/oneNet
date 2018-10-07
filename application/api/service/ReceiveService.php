cdc8a0d3f4877340ac9beabd339291708e44f5f9
[zhumingliang] <[mingliangzhu@aliyun.com]>
2018-10-08 00:38:15 +0800

    测试接受数据

--- application/api/service/ReceiveService.php
+++ application/api/service/ReceiveService.php
@@ -113,7 +113,7 @@ class ReceiveService
         try {
             $sendParams = self::preParams($params['imei'], $params['obj_id'], $params['obj_inst_id'],
                 $params['res_id'], $params['X'], $params['Y'], $params['threshold'], $params['interval']);
-           print_r($sendParams);
+
             $output = self::post($sendParams['url'], $sendParams['header'], $sendParams['content']);
             LogT::create(['msg' => $output]);
             $output_array = json_decode($output, true);
