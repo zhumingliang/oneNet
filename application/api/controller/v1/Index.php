12ef95c81c0ac0c634784ff349f7fa15f2ddc0b7
[zhumingliang] <[mingliangzhu@aliyun.com]>
2018-11-14 09:26:43 +0800

    X,Y修改格式

--- application/api/controller/v1/Index.php
+++ application/api/controller/v1/Index.php
@@ -39,7 +39,7 @@ class Index extends BaseController
                 //接受post数据
                 ReceiveService::save($resolved_body);
             }
-            echo $resolved_body;
+            return json(new SuccessMessage());
 
         }
     }
