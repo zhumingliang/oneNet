5b32d14e3c05555afe96163f53da23de183ea978
[zhumingliang] <[mingliangzhu@aliyun.com]>
2019-03-30 00:35:19 +0800

    X,Y修改格式

--- application/api/service/OneNet.php
+++ application/api/service/OneNet.php
@@ -48,8 +48,8 @@ class OneNet
         }
 
         $res = $sm->raw_response();
-        var_dump($res);
         $res=json_decode($res);
+        var_dump($res);
         $device_id = $res->data['device_id'];
         //保存到数据库
         $params['device_id'] = $device_id;
