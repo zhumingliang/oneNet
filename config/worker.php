992694571af7aa472d43721fb1f221eb29aac163
[zhumingliang] <[mingliangzhu@aliyun.com]>
2019-04-22 00:53:00 +0800

    X,Y修改格式

--- config/worker.php
+++ config/worker.php
@@ -18,7 +18,7 @@ return [
     'protocol'       => 'websocket', // 协议 支持 tcp udp unix http websocket text
     'host'                  => '0.0.0.0', // 监听地址
     'port'                  => 2346, // 监听端口
-    'root'                  => '/', // WEB 根目录 默认会定位public目录
+    'root'                  => '', // WEB 根目录 默认会定位public目录
     'app_path'              => '', // 应用目录 守护进程模式必须设置（绝对路径）
     'file_monitor'          => false, // 是否开启PHP文件更改监控（调试模式下自动开启）
     'file_monitor_interval' => 2, // 文件监控检测时间间隔（秒）
