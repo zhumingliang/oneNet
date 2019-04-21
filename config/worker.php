8f1570ad40444ca7b0ca5d10c801f5a4f167a9ef
[zhumingliang] <[mingliangzhu@aliyun.com]>
2019-04-22 00:35:27 +0800

    X,Y修改格式

--- config/worker.php
+++ config/worker.php
@@ -25,7 +25,7 @@ return [
     'file_monitor_path'     => [], // 文件监控目录 默认监控application和config目录
 
     // 支持workerman的所有配置参数
-    'name'                  => 'onenet',
+    'name'                  => 'thinkphp',
     'count'                 => 4,
     'daemonize'             => false,
     'pidFile'               => Env::get('runtime_path') . 'worker.pid',
