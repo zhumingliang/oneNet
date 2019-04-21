e045e9a4f5232e468764d941013ff82e9ba3802f
[zhumingliang] <[mingliangzhu@aliyun.com]>
2019-04-22 01:24:22 +0800

    X,Y修改格式

--- config/worker_server.php
+++ config/worker_server.php
@@ -15,7 +15,7 @@ use think\facade\Env;
 // +----------------------------------------------------------------------
 return [
     // 扩展自身需要的配置
-    'protocol'       => 'websocket', // 协议 支持 tcp udp unix http websocket text
+    'protocol'       => 'http', // 协议 支持 tcp udp unix http websocket text
     'host'           => '0.0.0.0', // 监听地址
     'port'           => 2345, // 监听端口
     'socket'         => '', // 完整监听地址
